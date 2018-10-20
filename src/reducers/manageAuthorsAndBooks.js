8a7e9c8f3b6130ed548c69adada428d7a581ce7e
github username <github email address>
2018-10-20 16:27:51 -0400

    Done.

--- src/reducers/manageAuthorsAndBooks.js
+++ src/reducers/manageAuthorsAndBooks.js
@@ -45,14 +45,40 @@
 //
 
 import { combineReducers } from "redux";
-import authorsReducer from './reducers/authorsReducer';
-import booksReducer from './reducers/booksReducer';
 
 const rootReducer = combineReducers({
-  books: booksReducer,
-  authors: authorsReducer
-})
-
-// const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+  authors: authorsReducer,
+  books: booksReducer
+});
 
 export default rootReducer;
+
+function booksReducer(state = [], action) {
+  let idx;
+  switch (action.type) {
+    case "ADD_BOOK":
+      return [...state, action.book];
+
+    case "REMOVE_BOOK":
+      idx = state.indexOf(action.id);
+      return [...state.slice(0, idx), ...state.slice(idx + 1)];
+
+    default:
+      return state;
+  }
+}
+
+function authorsReducer(state = [], action) {
+  let idx;
+  switch (action.type) {
+    case "ADD_AUTHOR":
+      return [...state, action.author];
+
+    case "REMOVE_AUTHOR":
+      idx = state.indexOf(action.id);
+      return [...state.slice(0, idx), ...state.slice(idx + 1)];
+
+    default:
+      return state;
+  }
+}
