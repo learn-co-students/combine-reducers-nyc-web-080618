b23fcfb26a5e7472c44a6f9374a21d51c611a78e
github username <github email address>
2018-10-20 16:25:18 -0400

    Done.

--- src/reducers/manageAuthorsAndBooks.js
+++ src/reducers/manageAuthorsAndBooks.js
@@ -1,43 +1,58 @@
-export default function bookApp(state = {
-  authors: [],
-  books: []
-}, action) {
-  let idx
-  switch (action.type) {
+//Version 1
+// export default function bookApp(state = {
+//   authors: [],
+//   books: []
+// }, action) {
+//   let idx
+//   switch (action.type) {
+//
+//     case "ADD_BOOK":
+//       return {
+//         ...state,
+//         books: [...state.books, action.book]
+//       };
+//
+//     case "REMOVE_BOOK":
+//       idx = state.books.indexOf(action.id);
+//       return {
+//         ...state,
+//         books: [
+//           state.books.slice(0, idx),
+//           state.books.slice(idx + 1),
+//         ]
+//       };
+//
+//     case "ADD_AUTHOR":
+//         return {
+//           ...state,
+//           authors: [...state.authors, action.author]
+//         };
+//
+//     case "REMOVE_AUTHOR":
+//       idx = state.authors.indexOf(action.id);
+//       return {
+//         ...state,
+//         authors: [
+//           state.authors.slice(0, idx),
+//           state.authors.slice(idx + 1)
+//         ]
+//       };
+//
+//     default:
+//       return state;
+//     }
+// };
+//
 
-    case "ADD_BOOK":
-      return {
-        ...state,
-        books: [...state.books, action.book]
-      };
+import { combineReducers } from "redux";
+import authorsReducer from './reducers/authorsReducer';
+import booksReducer from './reducers/booksReducer';
 
-    case "REMOVE_BOOK":
-      idx = state.books.indexOf(action.id);
-      return {
-        ...state,
-        books: [
-          state.books.slice(0, idx),
-          state.books.slice(idx + 1),
-        ]
-      };
+const rootReducer = combineReducers({
+  books: booksReducer,
+  authors: authorsReducer
+})
 
-    case "ADD_AUTHOR":
-        return {
-          ...state,
-          authors: [...state.authors, action.author]
-        };
+// const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
-    case "REMOVE_AUTHOR":
-      idx = state.authors.indexOf(action.id);
-      return {
-        ...state,
-        authors: [
-          state.authors.slice(0, idx),
-          state.authors.slice(idx + 1)
-        ]
-      };
-
-    default:
-      return state;
-    }
-};
+export default rootReducer;
